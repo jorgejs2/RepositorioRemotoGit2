ecd32692328f566f41b56a4a40ac42cc746aacc6
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-17 17:14:06 +0100

    1er Commit rama1 2 prints

--- Proyecto2/src/paquete1/Principal.java
+++ Proyecto2/src/paquete1/Principal.java
@@ -3,5 +3,7 @@ package paquete1;
 public class Principal {
 	public static void main(String args[]) {
 	System.out.println("Hola");
+	System.out.println("Escrito desde la primera rama");
+	System.out.println("Antes del merge");
 	}
 }
